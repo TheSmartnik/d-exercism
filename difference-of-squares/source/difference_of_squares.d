2eaafb108584597d16fd2e0e1fe90c1ff26203d4
 TheSmartnik <misharinn@gmail.com>
2018-04-26 19:30:39 +0300

    Super version with ranges & algorithms

--- difference-of-squares/source/difference_of_squares.d
+++ difference-of-squares/source/difference_of_squares.d
@@ -1,27 +1,20 @@
 module difference_of_squares;
 
+import std.range : iota;
+import std.algorithm : sum, reduce;
 import std.stdio;
 
 struct squares {
     int square;
 
     ulong squareOfSum() {
-        int sum = 0;
-
-        for(int i = 0; i <= square; i++)
-            sum += i;
-
-        return sum * sum;
+        int s = iota(1, square + 1).sum();
+        return s ^^ 2;
     }
 
     ulong sumOfSquares()
     {
-        int sum = 0;
-
-        for(int i = 0; i <= square; i++)
-            sum += i * i;
-
-        return sum;
+        return reduce!((a, b) => a + b * b)(0, iota(0, square + 1));
     }
 
     ulong difference()
@@ -38,10 +31,10 @@ const int allTestsEnabled = 1;
     assert(squares(100).squareOfSum == 25_502_500);
 
     assert(squares(5).sumOfSquares == 55);
+static if (allTestsEnabled) {
     assert(squares(10).sumOfSquares == 385);
     assert(squares(100).sumOfSquares == 338_350);
 
-static if (allTestsEnabled) {
     assert(squares(0).difference == 0);
     assert(squares(5).difference == 170);
     assert(squares(10).difference == 2_640);
